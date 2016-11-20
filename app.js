c4e648455ff9426734bc370129de72190b45063c
Sophie Wang <hereissophie@gmail.com>
2016-11-19 20:33:49 -0500

    added url routing to index page in app.js

--- app.js
+++ app.js
@@ -16,19 +16,12 @@ mongoose.connect(process.env.DB_URI, function (err) {
 // Node startup
 var app = express();
 app.use(bodyParser.json());
-<<<<<<< HEAD
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(__dirname + '/public/assets'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Get the index page:
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
-
-=======
-app.use(bodyParser.urlencoded({extended: false}));
->>>>>>> 75bbc03a4c71ba9ac06c7f6c6ab3d54a9b771501
 
 // Routing
 app.use(require('./app/routes'));
